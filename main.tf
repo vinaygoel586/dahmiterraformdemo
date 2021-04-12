cf6abf0f315c5e28a688b32bacb8a761ecf8df03
vinaygoel586 <59254605+vinaygoel586@users.noreply.github.com>
2021-04-12 11:59:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = ["amazon"]
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-20151015"]
-  }
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
-  
-}              
+        
  
 resource "aws_instance" "webserver" {
   ami             = "ami-042e8287309f5df03"
